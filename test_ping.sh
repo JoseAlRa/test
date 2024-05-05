6be21375f999ef2bee4e7edc53c6adb52d5449b9
JoseAlRa <150192279+JoseAlRa@users.noreply.github.com>
2024-05-05 01:19:30 -0600

    Update test_ping.sh

--- test_ping.sh
+++ test_ping.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-# Array de direcciones IP de los equipos
-ip_addresses=(
-    "192.168.0.110"
-    "192.168.0.104"
-    "192.168.0.106"
-    "192.168.0.113"
-    # Agrega aquí todas las direcciones IP que necesites
-)
-
-# Iterar sobre las direcciones IP y hacer ping a cada una
-for ip in "${ip_addresses[@]}"
-do
-    echo "Haciendo ping a $ip"
-    ping -c 4 $ip  # Hace ping cuatro veces a cada dirección IP
-done
+ls /
